8b154b0dea84cd09c0a8e1ab6e1a111bc7fbb2c2
Laura Lebon <bal-esotn59.fr@sogeti.com>
2019-09-17 11:40:20 +0200

    Commit avec Challenge (thx Jorys)

--- src/test/java/tests/TestManager.java
+++ src/test/java/tests/TestManager.java
@@ -1,8 +1,16 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
@@ -18,6 +26,9 @@ public class TestManager {
 	static AppiumDriver<MobileElement> driver;
 	DesiredCapabilities capabilities ;
 	
+	String folder_name;
+	DateFormat df;
+	
 	@BeforeSuite
 	public void setup() {
 		try {
@@ -28,8 +39,8 @@ public class TestManager {
 		    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"8.0.0");
 		    capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
 		    //capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
-		    //capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/llebon/downloads/KABOOM_v4.0.104_apkpure.com.apk");
-		    capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/llebon/downloads/Findrive_apkpure.com.apk");
+			capabilities.setCapability("appPackage", "io.appium.android.apis");
+			capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
 		    
 		}
 		catch (Exception e) {
@@ -56,5 +67,17 @@ public class TestManager {
 	@AfterSuite
 	public void SuiteTeardown() {
 		driver.quit();
+	}
+	
+	// Actions disponibles pour tous les cas de test : 
+	
+	public void captureScreenShots() throws IOException {
+		folder_name = "screenshot";
+		File f=( (TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		df = new  SimpleDateFormat("dd-MMM-yyyy__hh_mm_ssaa");
+		new File(folder_name).mkdir();
+		String file_name=df.format(new Date())+".png";
+		FileUtils.copyFile(f, new File(folder_name + "/" + file_name));
+		System.out.println("La capture d'écran a fonctionné.");
 	}
 }
--- src/test/java/tests/Testsuite.java
+++ src/test/java/tests/Testsuite.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -37,5 +38,24 @@ public class Testsuite extends TestManager{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void testChallenge() throws Exception {
+		// Click on 'App'.
+		driver.findElementByAccessibilityId("App").click();
+		System.out.println("Le click sur App a fonctionné.");
+		// Click on 'Fragment'.
+		driver.findElementByAccessibilityId("Fragment").click();
+		System.out.println("Le click sur Fragment a fonctionné.");
+		// Click on 'Layout'.
+		driver.findElementByAccessibilityId("Layout").click();
+		System.out.println("Le click sur Layout a fonctionné.");
+		// Vérification de la présence de l'option "Othello".
+		String text = driver.findElement(By.xpath("//*[@text = 'Othello']")).getText();
+		Assert.assertEquals(text, "Othello");
+		System.out.println("L'option 'Othello' est bien présente.");
+		//Take a Screenshot.
+		captureScreenShots();
 	}
 }
